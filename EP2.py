c5d238593af2576cda13eba49c420fd615125948
Enrico-Hlebanja <Enricoihlebanja@gmail.com>
2023-04-19 09:24:57 -0300

    Define_posicoes

--- EP2.py
+++ EP2.py
@@ -1,1 +1,16 @@
-print('hello world')
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    grid=[0]*10
+    Tabuleiro = [grid]*10
+    origem = [linha, coluna]
+    posiçoes_do_barco = []
+    if orientacao == 'vertical':
+        for e in range(tamanho): 
+            origem = [linha, coluna]
+            posiçoes_do_barco.append(origem)
+            linha+=1
+    if orientacao == 'horizontal':
+        for e in range(tamanho): 
+            origem = [linha, coluna]
+            posiçoes_do_barco.append(origem)
+            coluna+=1
+    return posiçoes_do_barco
