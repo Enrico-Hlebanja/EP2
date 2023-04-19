8ec02a93cc53b73857c32db4bf869ed164eb9cb8
Enrico-Hlebanja <Enricoihlebanja@gmail.com>
2023-04-19 13:36:32 -0300

    Preenche frota

--- EP2.py
+++ EP2.py
@@ -1,17 +1,23 @@
-
 def define_posicoes(linha,coluna,orientacao,tamanho):
-    grid=[0]*10
-    Tabuleiro = [grid]*10
-    origem = [linha, coluna]
-    posiçoes_do_barco = []
-    if orientacao == 'vertical':
-        for e in range(tamanho): 
-            origem = [linha, coluna]
-            posiçoes_do_barco.append(origem)
-            linha+=1
-    if orientacao == 'horizontal':
-        for e in range(tamanho): 
-            origem = [linha, coluna]
-            posiçoes_do_barco.append(origem)
-            coluna+=1
-    return posiçoes_do_barco
+        grid=[0]*10
+        Tabuleiro = [grid]*10
+        origem = [linha, coluna]
+        posiçoes_do_barco = []
+        if orientacao == 'vertical':
+            for e in range(tamanho): 
+                origem = [linha, coluna]
+                posiçoes_do_barco.append(origem)
+                linha+=1
+        if orientacao == 'horizontal':
+            for e in range(tamanho): 
+                origem = [linha, coluna]
+                posiçoes_do_barco.append(origem)
+                coluna+=1
+        return posiçoes_do_barco
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    if nome_navio not in frota:
+         frota[nome_navio] = []
+
+    frota[nome_navio].append(define_posicoes(linha,coluna,orientacao,tamanho))
+
+    return frota
